e36cf465e588c75b82482243eaa835768d9b2fe6
Newton4468 <Newtty.8644@gmail.com>
2025-09-26 10:25:58 +0300

    Create Programs.c

---
+++ Programs.c
@@ -0,0 +1,22 @@
+/*
+Name: kipsemian Newton
+Reg no: pa106/g/28743/25
+Descripton: c program to display software engineering programs 
+*/
+
+#include <stdio.h>
+
+int main(){
+	printf ("SOFTWARE ENGINEERING UNITS SEM 1\n");
+	printf ("1. Introduction to programing and problem solving\n");
+	printf("2. Fundamentals of system analysis and desing\n");
+  printf ("3. Discrete mathematics\n");
+  printf ("4. Computer systems and organization\n");
+  printf ("5. Mathematics for science\n");
+  printf ("6. Fundermentals of ict\n");
+  printf ("7. Communication and information literacy\n");
+       return 0;
+       }
+       
+
+       
