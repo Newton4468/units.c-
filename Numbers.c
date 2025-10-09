9141fda1e34d1b4d5f6d9f051bd50c0083f01d18
Newton4468 <Newtty.8644@gmail.com>
2025-10-09 18:26:30 +0300

    Create Numbers.c

---
+++ Numbers.c
@@ -0,0 +1,20 @@
+/*
+Name: kipsemian Newton
+Reg no: pa106/g/28743/25
+Description: c program displaying numbers from 100_50 using for loop
+*/
+
+#include<stdio.h>
+//start,stop, step
+int main(){
+//decrement from 100_50 by 1
+
+int i=100;
+
+for (i<=100; i>=50;--i){
+
+printf(" %d\n",i);}
+
+
+return 0;
+}
