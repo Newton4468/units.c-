f9282253f9d69cf4273e7003ba06d27c53a2477f
Newton4468 <Newtty.8644@gmail.com>
2025-10-21 14:35:20 +0300

    Create convertTocelcius.c

---
+++ convertTocelcius.c
@@ -0,0 +1,29 @@
+/*
+Name: kipsemian Newton
+Reg no: pa106/g/28743/25
+Description: c function convertTocelcius
+*/
+
+#include<stdio.h>
+//function convertTocelcius 
+float convertTocelcius (float temperature);
+int main(){
+float temperature;
+float celcius;
+
+printf ("Enter the temperature in Fahrenheit ");
+scanf("%f",&temperature);
+
+celcius =convertTocelcius (temperature);
+
+printf("The celcius is %. 2f °c",celcius );
+
+}
+ float convertTocelcius (float temperature){
+
+ float celcius;
+ celcius =(temperature-32) * 5.0/9.0;
+ 
+ return celcius; 
+ 
+ }
