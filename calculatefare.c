07a8adf140d121325d94aa7f3b5f0d5b1e95e1dd
Newton4468 <Newtty.8644@gmail.com>
2025-10-21 14:33:56 +0300

    Create calculatefare.c

---
+++ calculatefare.c
@@ -0,0 +1,26 @@
+/*
+Name: kipsemian Newton
+Reg no: pa106/g/28743/25
+Description: c function to calculatefare 
+*/
+
+#include<stdio.h>
+//function to calculate fare
+
+int calculatefare (int kilometers);
+
+int main(){
+ int kilometers;
+ int fare;
+ printf("Enter distance in kilometers covered  ");
+ scanf("%d",&kilometers);
+
+  fare = calculatefare(kilometers);
+printf ("The total fare is %dksh",fare);
+}
+
+ int  calculatefare(int kilometers){
+  int fare;
+ fare = kilometers * 50;  
+ return fare;
+
